ca7dcdbc11bed42925c46868fb4c7a6fc7c567a0
Kauanny Rosa Moraes <kauannymoraes.ss055@academico.ifsul.edu.br>
2023-11-10 08:25:29 -0300

    Spring

---
+++ src/main/java/com/example/trabalholp/TrabalhoDaniel4KApplication.java
@@ -0,0 +1,13 @@
+package com.example.trabalholp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoDaniel4KApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoDaniel4KApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trabalholp/TrabalhoDaniel4KApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trabalholp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoDaniel4KApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
